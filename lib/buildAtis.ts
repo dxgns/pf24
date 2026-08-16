55ec306eaa4e4951de6e2124e523accaa1962b76
dxgns <norambuenasolisd@gmail.com>
2026-08-16 14:05:17 -0400

    Include manual transition values in ATIS text

--- lib/buildAtis.ts
+++ lib/buildAtis.ts
@@ -6,6 +6,8 @@ export function buildAtisText({
   approachOptional,
   departureRunway,
   arrivalRunway,
+  transitionAltitude,
+  transitionLevel,
   extraInfo,
   remarks,
 }: {
@@ -16,12 +18,17 @@ export function buildAtisText({
   approachOptional?: string;
   departureRunway: string;
   arrivalRunway: string;
+  transitionAltitude?: string;
+  transitionLevel?: string;
   extraInfo?: string;
   remarks?: string;
 }) {
   const optionalApproach = approachOptional ? ` O ${approachOptional}` : "";
+  const transition = transitionAltitude && transitionLevel
+    ? ` TRANS ALT ${transitionAltitude} TRANS LVL ${transitionLevel}`
+    : "";
   const extra = extraInfo ? ` ${extraInfo}` : "";
   const rmk = remarks ? ` RMK: ${remarks}` : "";
 
-  return `[${airport}] ATIS INFO ${info}... (${metar})... AERONAVES ESPEREN APPR ${approachPrimary}${optionalApproach} SALIDAS PISTA ${departureRunway} LLEGADAS PISTA ${arrivalRunway} XPDR MODO ALT EN TODAS LAS CALLES DE RODAJE Y PISTAS EN USO... NOTIFIQUE INFO ${info} EN CONTACTO INICIAL${extra}${rmk}`;
+  return `[${airport}] ATIS INFO ${info}... (${metar})... AERONAVES ESPEREN APPR ${approachPrimary}${optionalApproach} SALIDAS PISTA ${departureRunway} LLEGADAS PISTA ${arrivalRunway}${transition} XPDR MODO ALT EN TODAS LAS CALLES DE RODAJE Y PISTAS EN USO... NOTIFIQUE INFO ${info} EN CONTACTO INICIAL${extra}${rmk}`;
 }
