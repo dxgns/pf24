049edf506706240804fcef6392aadda19ec88e3b
epilef536 <felipecacheda2011@gmail.com>
2026-08-18 20:57:49 -0300

    scope: remove legacy radar map background

--- app/scope/layout.tsx
+++ app/scope/layout.tsx
@@ -1,11 +1,5 @@
 import type { ReactNode } from "react";
-import ScopeRadarMapV2 from "@/components/scope/ScopeRadarMapV2";
 
 export default function ScopeLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      {children}
-      <ScopeRadarMapV2 />
-    </>
-  );
+  return children;
 }
