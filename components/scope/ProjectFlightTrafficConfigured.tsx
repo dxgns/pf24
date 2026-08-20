aaf563e9d93365c132679f70a0c484ebf7d6cc14
epilef536 <felipecacheda2011@gmail.com>
2026-08-20 07:57:14 -0300

    Enable calibrated traffic coordinate shim

--- components/scope/ProjectFlightTrafficConfigured.tsx
+++ components/scope/ProjectFlightTrafficConfigured.tsx
@@ -2,6 +2,7 @@
 
 import ProjectFlightTrafficV6 from "@/components/scope/ProjectFlightTrafficV6";
 import TrafficCalibrationDebug from "@/components/scope/TrafficCalibrationDebug";
+import { installTrafficCalibrationShim } from "@/components/scope/TrafficCalibrationShim";
 import type { ScopeFlightPlan } from "@/lib/scope/types";
 
 type Props = {
@@ -10,6 +11,8 @@ type Props = {
 };
 
 export default function ProjectFlightTrafficConfigured({ initialPlans, serverId }: Props) {
+  installTrafficCalibrationShim();
+
   return <>
     <ProjectFlightTrafficV6 initialPlans={initialPlans} serverId={serverId} />
     <TrafficCalibrationDebug serverId={serverId} />
