2e9414462992757f2d372ca418f8bb653e4a6a39
epilef536 <felipecacheda2011@gmail.com>
2026-08-21 23:54:11 -0300

    Apply APP shared-edge dedup in Scope

--- app/scope/page.tsx
+++ app/scope/page.tsx
@@ -26,6 +26,7 @@ import ScopeTrafficFooterPlacement from "@/components/scope/ScopeTrafficFooterPl
 import ScopePersonalization from "@/components/scope/ScopePersonalization";
 import RadarViewport from "@/components/scope/RadarViewport";
 import ScopeRadarMap from "@/components/scope/ScopeRadarMap";
+import ScopeAirspaceBoundaryDedup from "@/components/scope/ScopeAirspaceBoundaryDedup";
 import MdstSvgAirport from "@/components/scope/MdstSvgAirport";
 import MdabSvgAirport from "@/components/scope/MdabSvgAirport";
 import MdcrMtcaSvgAirport from "@/components/scope/MdcrMtcaSvgAirport";
@@ -64,7 +65,7 @@ export default async function ScopePage() {
   return <>
     <ScopeNativeListCss /><PF24Scope initialPlans={plans} controllerName={controllerName} />
     <ScopeConnectionPersistence /><ScopeConnectDialogPersistence /><ScopeOwnedTrafficLifecycle /><ScopeTrafficAutoRelease />
-    <ScopeLayoutGuards /><WeatherPanelV2 /><ScopeRadarMap /><MdstSvgAirport /><MdabSvgAirport /><MdcrMtcaSvgAirport />
+    <ScopeLayoutGuards /><WeatherPanelV2 /><ScopeRadarMap /><ScopeAirspaceBoundaryDedup /><MdstSvgAirport /><MdabSvgAirport /><MdcrMtcaSvgAirport />
     <ProjectFlightTrafficConfigured initialPlans={plans} serverId={projectFlightServerId} />
     <ScopeTrafficSettings /><ScopeTrafficLabelUX /><ScopeTrafficMappState /><ScopeTrafficHandover initialPlans={plans} /><ScopeHandoverAudioAlerts />
     <ScopeUnplannedHold initialPlans={plans} /><ScopeUnplannedTrafficOperationsV4 initialPlans={plans} /><ScopeSharedHoldSync />
