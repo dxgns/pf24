787173dba8a9ad5d8f0400037391d3b167ce786a
epilef536 <felipecacheda2011@gmail.com>
2026-08-22 20:26:13 -0300

    Mount conflict color guard

--- app/scope/page.tsx
+++ app/scope/page.tsx
@@ -42,6 +42,7 @@ import ScopeQdmTool from "@/components/scope/ScopeQdmTool";
 import ScopePlannedRouteTool from "@/components/scope/ScopePlannedRouteTool";
 import ScopeMapDisplayFilters from "@/components/scope/ScopeMapDisplayFilters";
 import ScopeConflictDetection from "@/components/scope/ScopeConflictDetection";
+import ScopeConflictColorGuard from "@/components/scope/ScopeConflictColorGuard";
 import ScopeRatingAccess from "@/components/scope/ScopeRatingAccess";
 import ScopeUiRefinements from "@/components/scope/ScopeUiRefinements";
 import ScopeUiConsistencyFixes from "@/components/scope/ScopeUiConsistencyFixes";
@@ -80,7 +81,7 @@ export default async function ScopePage() {
     <ScopeTrafficSettings /><ScopeTrafficLabelUX /><ScopeTrafficMappState /><ScopeTrafficHandover initialPlans={plans} /><ScopeHandoverAudioAlerts />
     <ScopeUnplannedHold initialPlans={plans} /><ScopeUnplannedPlanOwnershipBridge initialPlans={plans} /><ScopeUnplannedTrafficOperationsV4 initialPlans={plans} /><ScopeSharedHoldSync />
     <ScopeTrafficOperations initialPlans={plans} /><ScopeHoldTelemetry /><ScopeTrafficOwnershipVisuals initialPlans={plans} /><ScopeTrafficOwnershipColorGuard /><ScopeSectorOwnershipVisuals initialPlans={plans} />
-    <ScopeTrafficFooterPlacement /><ScopePersonalization /><RadarViewport /><ScopeFunctionalExtras /><ScopeQdmTool /><ScopeMapDisplayFilters /><ScopePlannedRouteTool initialPlans={plans} /><ScopeConflictDetection initialPlans={plans} />
+    <ScopeTrafficFooterPlacement /><ScopePersonalization /><RadarViewport /><ScopeFunctionalExtras /><ScopeQdmTool /><ScopeMapDisplayFilters /><ScopePlannedRouteTool initialPlans={plans} /><ScopeConflictDetection initialPlans={plans} /><ScopeConflictColorGuard />
     <ScopeRatingAccess roles={session.user?.discordRoles ?? []} /><ScopeUiRefinements /><ScopeUiConsistencyFixes />
     <ScopeChromeAdditions /><ScopeInterfaceCorrections /><ScopeAtisDialogV2 controllerName={controllerName} />
     <ScopeAtisJurisdictionGuard controllerName={controllerName} /><ScopeAtisDisconnectCleanup controllerName={controllerName} />
