4048fc225b3441d16daec84335e6d4312adeb9c5
dxgns <norambuenasolisd@gmail.com>
2026-08-23 00:57:45 -0400

    fix: remove runtime package import from OpenNext config

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,4 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-
-export default defineCloudflareConfig({
+export default {
   enableCacheInterception: false,
   routePreloadingBehavior: "none",
-});
+};
