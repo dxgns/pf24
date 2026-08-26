dcd73baecd397fece56a612d55e4e69e26ad7e60
epilef536 <felipecacheda2011@gmail.com>
2026-08-25 22:33:50 -0300

    Fix CTR filter across full Scope map

--- components/scope/ScopeMapFilterCorrections.tsx
+++ components/scope/ScopeMapFilterCorrections.tsx
@@ -1,21 +1,9 @@
 "use client";
 
-import { AIRSPACE_PATHS } from "@/lib/scope/mapData";
-
-const ctrSelectors = AIRSPACE_PATHS
-  .filter((airspace) => airspace.tone === "fir")
-  .map((airspace, index) =>
-    airspace.id.endsWith("_CTR")
-      ? `[data-pf24-map-filter-ctrs='off'] [data-map-layer='fir-airspace'] > polyline:nth-child(${index + 1})`
-      : null,
-  )
-  .filter((selector): selector is string => selector !== null)
-  .join(",\n");
-
 export default function ScopeMapFilterCorrections() {
   return (
     <style>{`
-      ${ctrSelectors} {
+      [data-pf24-map-filter-ctrs='off'] [data-map-layer='fir-airspace'] {
         display: none !important;
       }
 
